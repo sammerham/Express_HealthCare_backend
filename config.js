29d03c23054f013bb401012ade67057ce7eb87f2
Sam Merham <sammerham@icloud.com>
2022-04-27 18:54:37 -0700

    prepare heroku depoloy

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 const DB_URI = process.env.NODE_ENV === "test"
   ? "postgresql:///doctorsdb_test"
-  : "postgresql:///doctorsdb";
+  : process.env.DATABASE_URL || "postgresql:///doctorsdb";
 
 const SECRET_KEY = process.env.SECRET_KEY || "secret";
 const JWT_OPTIONS = { expiresIn: 60 * 60 }; // 1 hour
@@ -14,3 +14,5 @@ module.exports = {
   JWT_OPTIONS,
   BCRYPT_WORK_FACTOR
 };
+
+
